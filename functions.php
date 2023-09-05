eda77887dbf87c8d55ef53ac7cae80f330eed294
Dimitar <dimikalapocev@gmail.com>
2023-09-05 20:11:08 +0200

    Add dynamic title tag

--- functions.php
+++ functions.php
@@ -4,6 +4,8 @@ add_action('wp_enqueue_scripts', function() {
     wp_enqueue_style('styles', get_stylesheet_directory_uri() . '/assets/styles/styles.css');
 
     wp_enqueue_script('main', get_stylesheet_directory_uri() . '/assets/scripts/index.js');
-})
+});
 
-?>
+add_action('after_setup_theme', function() {
+    add_theme_support('title-tag');
+});
