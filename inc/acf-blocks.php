8fcd3cc127f38fc19cf684ba7d7773c02bd7c82a
DimitarK13 <dimikalapocev@gmail.com>
2023-09-20 09:55:46 +0200

    Change hero -> hero-section

--- inc/acf-blocks.php
+++ inc/acf-blocks.php
@@ -5,10 +5,10 @@ add_action('acf/init', 'my_acf_blocks');
 function my_acf_blocks() {
     if( function_exists('acf_register_block_type') ) {
         acf_register_block_type(array(
-            'name' => 'hero',
+            'name' => 'hero-section',
             'title' => __('Block: Hero Section'),
             'description' => __('Custom Hero Section'),
-            'render_template' => '/blocks/hero/hero.php',
+            'render_template' => '/blocks/hero-section/hero-section.php',
             'category' => 'custom',
             'icon' => 'admin-comments',
             'keywords' => array('hero')
