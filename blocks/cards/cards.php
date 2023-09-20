93ac5127d5d604168a5bdacb86fc594cf8679ca4
DimitarK13 <dimikalapocev@gmail.com>
2023-09-20 09:24:52 +0200

    Add Cards layout

--- blocks/cards/cards.php
+++ blocks/cards/cards.php
@@ -1,36 +1,40 @@
 <?php
     $fields = get_fields();
-	extract($fields);
+    if(!empty($fields)) : extract($fields); else : return; endif;
 ?>
 
-<div class="cards__column">
-    <?php if (!empty($cards)) : ?>
-        <?php foreach ($cards as $card) : ?>
+<?php if (!empty($cards)) : ?>
+    <section class="container" aria-label="Benefits">
+        <div class="cards">
+            <?php foreach($cards as $cards_column) : ?>
+                <div class="cards__column">
+                    <?php foreach ($cards_column['cards_row'] as $card) : ?>
+                        <div class="card">
+                            <?php if(!empty($card['heading'])) : ?>
+                                <img
+                                    class="card__image"
+                                    src="<?php echo $card['image']['url'] ?>"
+                                    alt="<?php echo $card['image']['alt'] ?>"
+                                />
+                            <?php endif; ?>
 
-            <div class="card">
-                <?php if(!empty($card['heading'])) : ?>
-                    <img
-                        class="card__image"
-                        src="<?php echo $card['image']['url'] ?>"
-                        alt="<?php echo $card['image']['alt'] ?>"
-                    />
-                <?php endif; ?>
-
-                <div class="card__content">
-                    <?php if(!empty($card['heading'])) : ?>
-                        <h2 class="card__content-heading">
-                            <?php echo $card['heading']; ?>
-                        </h2>
-                    <?php endif; ?>
-                    
-                    <?php if(!empty($card['text'])) : ?>
-                        <p class="card__content-paragraph">
-                        <?php echo $card['text']; ?>
-                        </p>
-                    <?php endif; ?>
+                            <div class="card__content">
+                                <?php if(!empty($card['heading'])) : ?>
+                                    <h2 class="card__content-heading">
+                                        <?php echo $card['heading']; ?>
+                                    </h2>
+                                <?php endif; ?>
+                                
+                                <?php if(!empty($card['text'])) : ?>
+                                    <p class="card__content-paragraph">
+                                    <?php echo $card['text']; ?>
+                                    </p>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+                    <?php endforeach; ?>
                 </div>
+            <?php endforeach; ?>
             </div>
-
-        <?php endforeach; ?>
-    <?php endif; ?>
-</div>
+    </section>
+<?php endif; ?>
